b276343948e2d6d9eb9fa0bbaf2ecf2ea03eebc6
neptumiun <tymothe.billerey@gmail.com>
2025-01-04 11:17:47 +0100

    feat: add implementation of elgamal

--- elgamal.py
+++ elgamal.py
@@ -8,32 +8,37 @@ PARAM_Q = 0x8CF83642A709A097B447997640129DA299B1A47D1EB3750BA308B0FE64F5FBD3
 PARAM_G = 0x3FB32C9B73134D0B2E77506660EDBD484CA7B18F21EF205407F4793A1A0BA12510DBC15077BE463FFF4FED4AAC0BB555BE3A6C1B0C6B47B1BC3773BF7E8C6F62901228F8C28CBB18A55AE31341000A650196F931C77A57F2DDF463E5E9EC144B777DE62AAAB8A8628AC376D282D6ED3864E67982428EBC831D14348F6F2F9193B5045AF2767164E1DFC967C1FB3F2E55A4BD1BFFE83B9C80D052B985D182EA0ADB2A3B7313D3FE14C8484B1E052588B9B7D2BBD2DF016199ECD06E1557CD0915B3353BBB64E0EC377FD028370DF92B52C7891428CDC67EB6184B523D1DB246C32F63078490F00EF8D647D148D47954515E2327CFEF98C582664B4C0F6CC41659
 
 
-### call bruteLog with p = PARAM_P and g = PARAM_G
+def EG_generate_keys():
+    private_key = randint(1, PARAM_Q - 1)
+    public_key = pow(PARAM_G, private_key, PARAM_P)
+    return private_key, public_key
 
-def bruteLog(g, c, p):
-    s = 1
-    for i in range(p):
-        if s == c:
-            return i
-        s = (s * g) % p
-        if s == c:
-            return i + 1
-    return -1
+def EGM_encrypt(message, public_key):
+    # message_int = int.from_bytes(message.encode(), 'big')
+    k = randint(1, PARAM_Q - 1)
+    r = pow(PARAM_G, k, PARAM_P)
+    c = (message * pow(public_key, k, PARAM_P)) % PARAM_P
+    return r, c
 
-def EG_generate_keys("""TBC"""):
+def EGA_encrypt(message, public_key):
+    # message_int = int.from_bytes(message.encode(), 'big')
+    k = randint(1, PARAM_Q - 1)
+    r = pow(PARAM_G, k, PARAM_P)
+    c = (message + pow(public_key, k, PARAM_P)) % PARAM_P
+    return r, c
 
-    return """TBC"""
+def EG_decrypt(r, c, private_key):
+    s = pow(r, private_key, PARAM_P)
+    s_inv = mod_inv(s, PARAM_P)
+    decrypted_message_int = (c * s_inv) % PARAM_P
+    return int_to_bytes(decrypted_message_int)
 
-## multiplicative version
-def EGM_encrypt("""TBC"""):
+if __name__ == "__main__":
+    private_key, public_key = EG_generate_keys()
+    m1 = 0x2661b673f687c5c3142f806d500d2ce57b1182c9b25bfe4fa09529424b
+    m2 = 0x1c1c871caabca15828cf08ee3aa3199000b94ed15e743c3
+    r1, c1 = EGM_encrypt(m1, public_key)
+    r2, c2 = EGM_encrypt(m2, public_key)
 
-    return """TBC"""
-
-## additive version
-def EGA_encrypt("""TBC"""):
-    return """TBC"""
-
-
-def EG_decrypt("""TBC"""):
-    return """TBC"""
-
+    m3 = EG_decrypt(r1*r2, c1*c2, private_key)
+    print(m3)
